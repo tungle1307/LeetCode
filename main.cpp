24d7794f20e3bd9cc39c1c838dd6d25c60313e14
tungle <tungle1307@gmail.com>
2024-07-06 23:42:59 +0700

    LC-4 resolved median of two sort array

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "two_sum_1.h"
 #include "add_two_numbers_2.h"
 #include "longest_substring_3.h"
+#include "median_of_two_sort_array.h"
 using namespace std;
 
 int main() {
     TwoSum::testFunction();
     AddTwoNumber::testFunction();
     LongestSubstring::testFunction();
+    MediumOfTwoSortArray::testFunction();
     return 0;
 }
